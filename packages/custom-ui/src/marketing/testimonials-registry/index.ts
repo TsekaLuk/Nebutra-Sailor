4f8bf543c7e3ef99d6e9ce66487632868d82dfa5
Tseka Luk <contact@tsekaluk.dev>
2026-03-05 15:31:18 +0800

    chore: remove packages/custom-ui (migrated to design-system)

--- packages/custom-ui/src/marketing/testimonials-registry/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./types";
-export {
-  TestimonialsRegistry,
-  type TestimonialsRegistryProps,
-} from "./registry";
-export { StaggerTestimonialsAdapter } from "./stagger.adapter";
-export { Marquee3DTestimonials } from "./marquee3d";
-export { GridTestimonials, type GridTestimonialsProps } from "./grid-section";
--- packages/custom-ui/src/tokens/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { buttonTokens, type ButtonSize } from "./button";
-export { avatarTokens, type AvatarSize } from "./avatar";
-export { inputTokens } from "./input";
-export { cardTokens, type CardPadding } from "./card";
-export { badgeTokens } from "./badge";
-export { choiceboxTokens } from "./choicebox";
